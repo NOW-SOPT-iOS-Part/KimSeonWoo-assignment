53ad36dd30326a805c7e7a7e05988b903b788fbc
kim-seonwoo <sw204011@gmail.com>
2024-04-08 17:33:39 +0900

    [Set/#1] 프로젝트 세팅

---
+++ Tving-Clone/Tving-Clone/Presentation/Login/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  Tving-Clone
+//
+//  Created by Seonwoo Kim on 4/8/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
