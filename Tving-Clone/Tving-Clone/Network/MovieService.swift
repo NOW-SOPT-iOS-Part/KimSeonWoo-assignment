88fb3a41225b0d19d093eaf6b4737d5300a2c6d4
kim-seonwoo <sw204011@gmail.com>
2024-05-08 14:30:05 +0900

    [Set/#5] 네트워크 세팅

---
+++ Tving-Clone/Tving-Clone/Network/MovieService.swift
@@ -0,0 +1,83 @@
+//
+//  MovieService.swift
+//  Tving-Clone
+//
+//  Created by Seonwoo Kim on 5/8/24.
+//
+
+import Foundation
+
+import Moya
+
+final class MovieService {
+    static let shared = MovieService()
+    private var userProvider = MoyaProvider<MovieTargetType>(plugins: [MoyaLoggingPlugin()])
+    
+    private init() {}
+}
+
+extension MovieService {
+    func getUserInfo(memberId: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+        userProvider.request(.getUserInfo(memberId: memberId)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeStatus(by: statusCode, data, UserInfoResponseModel.self)
+                completion(networkResult)
+                
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+    
+    
+    func signUp(request: SignUpRequestModel, completion: @escaping (NetworkResult<Any>) -> Void) {
+        userProvider.request(.signUp(request: request)) { result in
+            switch result {
+            case .success(let response):
+                print("🫶 memberID는 \(String(describing: response.response?.allHeaderFields["Location"]))")
+                
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeStatus(by: statusCode, data, SignUpResponseModel.self)
+                completion(networkResult)
+                
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+    
+    
+    
+    
+    public func judgeStatus<T: Codable>(by statusCode: Int, _ data: Data, _ object: T.Type) -> NetworkResult<Any> {
+        
+        switch statusCode {
+        case 200..<205:
+            return isValidData(data: data, T.self)
+        case 400..<500:
+            return .requestErr
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    
+    private func isValidData<T: Codable>(data: Data, _ object: T.Type) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(T.self, from: data) else {
+            print("⛔️ \(self)애서 디코딩 오류가 발생했습니다 ⛔️")
+            return .pathErr }
+        
+        return .success(decodedData as Any)
+    }
+}
+
+
---
+++ Tving-Clone/Tving-Clone/Network/MovieTargetType.swift
@@ -0,0 +1,61 @@
+//
+//  MovieTargetType.swift
+//  Tving-Clone
+//
+//  Created by Seonwoo Kim on 5/8/24.
+//
+
+import Foundation
+
+import Moya
+
+
+enum MovieTargetType {
+    case getUserInfo(memberId: String)
+    case signUp(request: SignUpRequestModel)
+}
+
+extension MovieTargetType: TargetType {
+    var baseURL: URL {
+        return URL(string: Config.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .signUp:
+            return "/member/join"
+        case .getUserInfo(memberId: let memberId):
+            return "/member/info"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .signUp:
+            return .post
+        case .getUserInfo:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .signUp(let request):
+            return .requestJSONEncodable(request)
+        case .getUserInfo:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case.signUp:
+            return ["Content-Type": "application/json"]
+        case .getUserInfo(let memberId):
+            return ["Content-Type": "application/json",
+                    "memberId" : memberId]
+        }
+        
+    }
+}
+
