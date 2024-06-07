28678a3e07bcb6047560cf6883d717020a971c7f
kim-seonwoo <sw204011@gmail.com>
2024-06-07 21:36:04 +0900

    [Feat/#7] Entity 구현

---
+++ Tving-Clone/Tving-Clone/Domain/Entities/Content.swift
@@ -0,0 +1,20 @@
+//
+//  Movie.swift
+//  Tving-Clone
+//
+//  Created by Seonwoo Kim on 6/7/24.
+//
+
+import Foundation
+
+struct Content {
+    let data: [ContentData]
+}
+
+struct ContentData {
+    let image: String
+    var title: String
+    var maker: String
+    var subTitle: String
+    var ratio: String
+}
---
+++ Tving-Clone/Tving-Clone/Domain/Entities/ContentDetail.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetail.swift
+//  Tving-Clone
+//
+//  Created by Seonwoo Kim on 6/7/24.
+//
+
+import Foundation
+
+struct ContentDetail {
+    var title: String
+    var openDt: String
+    var directors: String
+    var actors: String
+    var audits: String
+    var companys: String
+}
