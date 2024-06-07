2fb947f10ff7959aeb36a7a443bd3749f0e196b8
kim-seonwoo <sw204011@gmail.com>
2024-06-07 21:34:47 +0900

    [Feat/#7] SceneDelegate 의존성 주입

--- Tving-Clone/Tving-Clone/Application/SceneDelegate.swift
+++ Tving-Clone/Tving-Clone/Application/SceneDelegate.swift
@@ -11,15 +11,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        self.window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: TabBarController())
-        self.window?.rootViewController = navigationController
-        self.window?.makeKeyAndVisible()
-    }
 
+        let contentDataSource = ContentDataSource()
+        let contentRepository = DefaultContentRepository(contentDataSource: contentDataSource)
+        let contentUseCase = DefaultContentUseCase(repository: contentRepository)
+        let mainContentViewModel = MainContentViewModel(contentUseCase: contentUseCase)
+        let mainContentVC = MainContentViewController(viewModel: mainContentViewModel)
+
+        let tabBarController = TabBarController(mainContentViewModel: mainContentViewModel)
+
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: tabBarController)
+        window?.makeKeyAndVisible()
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
